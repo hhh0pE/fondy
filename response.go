8f79f9b22182f3f0ee493676c7d5cd9fc2d681b3
Łukasz Opasiak <lo@idesign.center>
2019-01-22 11:38:05 +0100

    Change FinalResponse.CardBin type to interface{}

--- response.go
+++ response.go
@@ -1,16 +1,17 @@
 package fondy
 
+// FinalResponse contains fields which are available in fondy final payment response
 type FinalResponse struct {
-	OrderID        string `json:"order_id"`
-	MerchantID     int    `json:"merchant_id"`
-	Amount         string `json:"amount"`
-	Currency       string `json:"currency"`
-	OrderStatus    string `json:"order_status"`
-	ResponseStatus string `json:"response_status"`
-	Signature      string `json:"signature"`
-	MaskedCard     string `json:"masked_card"`
-	CardBin        int    `json:"card_bin"`
-	CardType       string `json:"card_type"`
+	OrderID        string      `json:"order_id"`
+	MerchantID     int         `json:"merchant_id"`
+	Amount         string      `json:"amount"`
+	Currency       string      `json:"currency"`
+	OrderStatus    string      `json:"order_status"`
+	ResponseStatus string      `json:"response_status"`
+	Signature      string      `json:"signature"`
+	MaskedCard     string      `json:"masked_card"`
+	CardBin        interface{} `json:"card_bin"`
+	CardType       string      `json:"card_type"`
 	// ResponseCode sometimes is int and sometimes string
 	ResponseCode        interface{} `json:"response_code,omitempty"`
 	ResponseDescription string      `json:"response_description"`
